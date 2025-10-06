3a1c62c3708576e52567eadc2b55f63aa0c0b064
Daniel Sánchez Cabello <dsancab968@ieszaidinvergeles.org>
2025-10-06 10:58:24 +0200

    Creacion de provision paraaa cliente y servidor
    Creamos los archivos para la provision de ambos y pequeña actualizacion vagrantfile
    
    Eliminacion archivo vacio

---
+++ provision/provision_client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Detenemos ejecucion si falla algun comando
+set -e
+
+#Actualizamos e instalamos el cliente y las herramientas de red
+apt update -y
+apt install -y net-tools iproute2 isc-dhcp-client
+
+#Mostramos mensaje de que todo ha ido bien y las interfaces ip para revisar que cada ciente tenga la ip correcta
+echo "Configuración de cliente lista."
+ip a
